f8529ffb63abdf0d18023f1184aa9703d174149c
9oodam <godamcity98@gmail.com>
2025-12-15 12:25:38 +0900

    feat: edit arg type

--- src/graphql/v2/resolvers/dashboard.ts
+++ src/graphql/v2/resolvers/dashboard.ts
@@ -1,7 +1,7 @@
-import type { Context, Root } from '@/types';
 import { DashboardV2Service } from '@/graphql/v2/services/dashboard.service';
+import type { Context, Root } from '@/types';
 
-export async function getDashboardV2Resolver(_root: Root, _args: {}, ctx: Context) {
+export async function getDashboardV2Resolver(_root: Root, _args: Record<string, never>, ctx: Context) {
   if (!ctx.userV2) {
     throw new Error('Unauthorized');
   }
@@ -9,12 +9,13 @@ export async function getDashboardV2Resolver(_root: Root, _args: {}, ctx: Contex
   const service = new DashboardV2Service(ctx.db, ctx.server);
   const userId = ctx.userV2.id;
 
-  const [hiringActivity, jobActivity, sponsorPaymentOverview, builderPaymentOverview] = await Promise.all([
-    service.getSponsorHiringActivity(userId),
-    service.getBuilderHiringActivity(userId),
-    service.getSponsorPaymentOverview(userId),
-    service.getBuilderPaymentOverview(userId),
-  ]);
+  const [hiringActivity, jobActivity, sponsorPaymentOverview, builderPaymentOverview] =
+    await Promise.all([
+      service.getSponsorHiringActivity(userId),
+      service.getBuilderHiringActivity(userId),
+      service.getSponsorPaymentOverview(userId),
+      service.getBuilderPaymentOverview(userId),
+    ]);
 
   return {
     hiringActivity,
